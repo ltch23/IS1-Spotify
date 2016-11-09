e618ed9716577681de32e0251c9e3027c570d675
Wil12Fred <Wil12Fred@192.168.1.24>
2016-11-09 17:11:40 -0500

    [Wilber] Agregando dominio Album.

--- src/main/java/dominio/Album.java
+++ src/main/java/dominio/Album.java
@@ -1,5 +1,16 @@
 package dominio;
 
-public class Album {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
+@Entity
+
+public class Album {
+	@Id
+	@SequenceGenerator(name = "album_id_generator", sequenceName = "album_id_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "album_id_generator")
 }
