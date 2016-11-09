8b5496078a9cd0d76a5cdffd19115fd91a00181e
Wilber Cutire Sivincha <wilbert_030@icloud.com>
2016-11-09 17:47:16 -0500

    Update Album.java
    Agregando Columnas

--- src/main/java/dominio/Album.java
+++ src/main/java/dominio/Album.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import java.util.Date;
 
 @Entity
 
@@ -13,4 +14,49 @@ public class Album {
 	@Id
 	@SequenceGenerator(name = "album_id_generator", sequenceName = "album_id_seq", allocationSize = 1)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "album_id_generator")
+	private Long id_album;
+	
+	@Column(length = 64)
+	private String nombre;
+	
+	@Column(nullable=false)
+	private boolean activo;
+	
+	@Column(nullable=false)
+	private Date date;
+	
+	@ManyToOne
+	private Artista sourceArtista;
+	
+	public String get_nombre(){
+		return nombre;
+	}
+	
+	public void set_activo(String nombre){
+		this.nombre=nombre;
+	}
+	
+	public boolean get_activo(){
+		return activo;
+	}
+	
+	public void set_activo(boolean activo){
+		this.activo=activo;
+	}
+	
+	public Date get_date(){
+		return date;
+	}
+	
+	public void setDate(Date date){
+		this.date=date;
+	}
+	
+	public Artista getSourceArtista(){
+		return sourceArtista;
+	}
+	
+	public void getSourceArtista(Artista sourceArtista){
+		this.sourceArtista=sourceArtista;
+	}
 }
